3753eae3ce653b116168c725a68329b74fdceebc
Anton Lantukh <lantukh@bk.ru>
2017-11-28 01:02:52 +0400

    Правки кода по кодстайлу

--- js/setup.js
+++ js/setup.js
@@ -1,36 +1,47 @@
 'use strict';
-// Показываем блок .setup
+
+// Ищем блок .setup и .setup-similar
 var setupBlock = document.querySelector('.setup');
-setupBlock.classList.remove('hidden');
-// Показываем блок .setup-similar
 var setupSimilarBlock = document.querySelector('.setup-similar');
-setupSimilarBlock.classList.remove('hidden');
-// Функция для определения случайного числа из диапозона
-function countRandom(min, max) {
-  return Math.floor((Math.random() * (max - min)) + min);
-}
+
 // Определяем массивы
-var wizardArray = [];
+var wizardsArray = [];
 var firstName = ['Иван', 'Хуан Себсатьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var secondName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-// Функция объединения создания волшебника
-function renderWizard(wizardName, wizardSurname, wizardCoatColor, wizardEyesColor) {
-  for (var i = 0; i < 4; i++) {
-    var wizardElement = {
-      name: wizardName[countRandom(0, 7)] + ' ' + wizardSurname[countRandom(0, 7)],
-      coatColor: wizardCoatColor[countRandom(0, 5)],
-      eyesColor: wizardEyesColor[countRandom(0, 4)]
-    };
-    wizardArray.push(wizardElement);
-  }
-  return wizardArray;
-}
+
 // Создаем элементы для DOM
-var template = document.querySelector('template');
-var setupElement = template.content.querySelector('.setup-similar-item');
+var setupElement = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var setupContainer = document.querySelector('.setup-similar-list');
+var fragment = document.createDocumentFragment();
+
+// Показываем элементы на странице
+setupBlock.classList.remove('hidden');
+setupSimilarBlock.classList.remove('hidden');
+
+// Наполняем массив волшебников
+for (var i = 0; i < 4; i++) {
+  wizardsArray.push(createWizard(firstName, secondName, coatColor, eyesColor));
+}
+
+// Отрисовывем волшебников через вызов функции в цикле
+for (i = 0; i < 4; i++) {
+  fragment.appendChild(renderWizardSetup(wizardsArray));
+}
+setupContainer.appendChild(fragment);
+
+
+// Функция создания волшебника
+function createWizard(name, surname, coat, eyes) {
+  var wizardElement = {
+    name: name[range(0, 7)] + ' ' + surname[range(0, 7)],
+    coatColor: coat[range(0, 5)],
+    eyesColor: eyes[range(0, 4)]
+  };
+  return wizardElement;
+}
+
 // Функция отображения волшебников
 function renderWizardSetup(wizard) {
   var wizardOutlook = setupElement.cloneNode(true);
@@ -39,9 +50,8 @@ function renderWizardSetup(wizard) {
   wizardOutlook.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardOutlook;
 }
-// Отрисовывем волшебников через вызов функции в цикле
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < 4; i++) {
-  fragment.appendChild(renderWizardSetup(renderWizard(firstName, secondName, coatColor, eyesColor)[i]));
+
+// Функция для определения случайного числа из диапозона
+function range(min, max) {
+  return Math.floor((Math.random() * (max - min)) + min);
 }
-setupContainer.appendChild(fragment);
