c03902782ccf8cd93e83a2906a62b0ab9c23f811
Anton Lantukh <lantukh@bk.ru>
2017-11-29 18:50:39 +0400

    Случайная смена цвета персонажа

--- js/setup.js
+++ js/setup.js
@@ -14,6 +14,7 @@ var firstName = ['Иван', 'Хуан Себсатьян', 'Мария', 'Кр�
 var secondName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+var fireballColor = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 // Создаем элементы для DOM
 var setupElement = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -61,17 +62,23 @@ var setupOpen = document.querySelector('.setup-open');
 var setupClose = document.querySelector('.setup-close');
 var setupInput = document.querySelector('.setup-user-name');
 var setupSubmit = document.querySelector('.setup-submit');
+var setupCoatColor = document.querySelector('.setup-wizard .wizard-coat');
+var setupEyesColor = document.querySelector('.setup-wizard .wizard-eyes');
+var setupFireball = document.querySelector('.setup-fireball-wrap');
 
+// Функция отображения окна настройки персонажа
 var openPopup = function () {
   setupBlock.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
 };
 
+// Функция сокрытия окна настройки персонажа
 var closePopup = function () {
   setupBlock.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
+// Функция обработки события при нажатии на ESC
 var onPopupEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     if (evt.target !== setupInput) {
@@ -82,34 +89,53 @@ var onPopupEscPress = function (evt) {
   }
 };
 
+// Открываем оконо при клике на аватар
 setupOpen.addEventListener('click', function () {
   openPopup();
 });
 
+// Открываем окно при нажатии на аватар
 setupOpen.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     openPopup();
   }
 });
 
+// Закрвыаем окно при клике на крестик
 setupClose.addEventListener('click', function () {
   closePopup();
 });
 
+// Закрываем окно при нажатии на крестик
 setupClose.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     closePopup();
   }
 });
 
-setupSubmit.addEventListener('click', function (evt) {
-  evt.preventDefault();
+// Закрываем окно при клике на сабмит
+setupSubmit.addEventListener('click', function () {
   closePopup();
 });
 
+// Закрываем окно при нажатии на сабмит
 setupSubmit.addEventListener('keydown', function (evt) {
-  evt.preventDefault();
   if (evt.keyCode === ENTER_KEYCODE) {
     closePopup();
   }
+});
+
+// Меняем цвет мантии при нажатии
+setupCoatColor.addEventListener('click', function () {
+  setupCoatColor.style.fill = coatColor[range(0, 5)];
+});
+
+// Меняем цвет глаз при нажатии
+setupEyesColor.addEventListener('click', function () {
+  setupEyesColor.style.fill = eyesColor[range(0, 4)];
+});
+
+// Меняем цвет фаербола при нажатии
+setupFireball.addEventListener('click', function () {
+  setupFireball.style.backgroundColor = fireballColor[range(0, 4)];
 });
